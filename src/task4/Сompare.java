3e0bcf7eb229edfd5b7f3016f04d1d60df82690b
Tima <velichkotimoha@gmail.com>
2025-10-15 17:04:22 +0300

    homework for 15.10.25

--- src/task4/Сompare.java
+++ src/task4/Сompare.java
@@ -2,6 +2,6 @@ package task4;
 
 public class Сompare {
     public static <T extends Comparable<T>> T minValue(T a, T b){
-        return a.compareTo(b) > 0 ? a : b;
+        return a.compareTo(b) > 0 ? b : a;
     }
 }
